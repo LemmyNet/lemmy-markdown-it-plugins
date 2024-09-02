4ca3338d531d70e2d80db0658509f7a667e50340
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-09-02 19:42:28 -0400

    Add more subscript tests

--- crates/markdown-it-sub/src/lib.rs
+++ crates/markdown-it-sub/src/lib.rs
@@ -36,6 +36,10 @@ mod test {
 
     #[rstest]
     #[case("~foo~", "<p><sub>foo</sub></p>\n")]
+    #[case("foo~", "<p>foo~</p>\n")]
+    #[case("~foo", "<p>~foo</p>\n")]
+    #[case("foo~bar~", "<p>foo<sub>bar</sub></p>\n")]
+    #[case("~~foo~bar~~~", "<p><s>foo<sub>bar</sub></s></p>\n")]
     fn test(#[case] md_str: &str, #[case] expected: &str) {
         let result = MARKDOWN_PARSER.parse(md_str).xrender();
 
