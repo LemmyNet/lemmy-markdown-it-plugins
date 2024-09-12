c5109362f85a26d6cc2827a0cd53664cd0202836
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-09-12 00:52:13 -0400

    Remove excess whitespaces

--- crates/markdown-it-ruby/src/lib.rs
+++ crates/markdown-it-ruby/src/lib.rs
@@ -14,8 +14,7 @@
 //! assert_eq!(html, String::from("<p><ruby>漢<rp>(</rp><rt>Kan</rt><rp>)</rp></ruby><ruby>字<rp>(</rp><rt>ji</rt><rp>)</rp></ruby></p>\n"));
 //! ```
 
-use std::char;
-
+use itertools::Itertools;
 use markdown_it::{
     parser::inline::{InlineRule, InlineState},
     MarkdownIt, Node, NodeValue, Renderer,
@@ -48,6 +47,33 @@ impl NodeValue for Ruby {
     }
 }
 
+fn find_text_end_index<const BREAK_CHAR: char>(
+    char_indices: &mut impl Iterator<Item = (usize, char)>,
+) -> Option<usize> {
+    let mut prev_char_escaped = false;
+
+    char_indices.find_map(|(i, c)| {
+        if c == '\\' {
+            prev_char_escaped = true;
+            return None;
+        }
+
+        let index = (c == BREAK_CHAR && !prev_char_escaped).then_some(i);
+        prev_char_escaped = false;
+
+        index
+    })
+}
+
+fn prepare_text(text: &str) -> String {
+    // Intersperse guarantees there are still spaces between visible text words.
+    #[allow(unstable_name_collisions)]
+    text.split_whitespace()
+        .intersperse(" ") // TODO: Use intersperse function from std once it makes it to a stable version: https://github.com/rust-lang/rust/issues/79524
+        .collect::<String>()
+        .replace('\\', "")
+}
+
 struct RubyScanner;
 
 impl InlineRule for RubyScanner {
@@ -59,42 +85,18 @@ impl InlineRule for RubyScanner {
             return None;
         }
 
-        let mut prev_char_escaped = false;
-
-        let base_end_pos = char_indices.find_map(|(i, c)| {
-            if c == '\\' {
-                prev_char_escaped = true;
-                return None;
-            }
-
-            let index = (c == '|' && !prev_char_escaped).then_some(i);
-            prev_char_escaped = false;
-
-            index
-        })? + state.pos;
-
+        let base_end_pos = find_text_end_index::<'|'>(&mut char_indices)? + state.pos;
         let base_text = &state.src[state.pos + 1..base_end_pos];
 
-        let ruby_end_pos = char_indices.skip(2).find_map(|(i, c)| {
-            if c == '\\' {
-                prev_char_escaped = true;
-                return None;
-            }
-
-            let index = (c == '}' && !prev_char_escaped).then_some(i);
-            prev_char_escaped = false;
-
-            index
-        })? + state.pos;
-
-        let ruby_text = &state.src[base_end_pos + 1..ruby_end_pos];
+        let end_pos = find_text_end_index::<'}'>(&mut char_indices.skip(2))? + state.pos;
+        let ruby_text = &state.src[base_end_pos + 1..end_pos];
 
         Some((
             Node::new(Ruby {
-                base_text: base_text.trim().into(),
-                ruby_text: ruby_text.trim().into(),
+                base_text: prepare_text(base_text),
+                ruby_text: prepare_text(ruby_text),
             }),
-            (ruby_end_pos - state.pos) + 1,
+            (end_pos - state.pos) + 1,
         ))
     }
 }
@@ -131,6 +133,18 @@ mod test {
     #[case(
         "{foo|bar}{baz\\|qux}",
         "<p><ruby>foo<rp>(</rp><rt>bar</rt><rp>)</rp></ruby>{baz|qux}</p>\n"
+    )]
+    #[case(
+        "{foo\\|bar\\}{baz|qux}",
+        "<p><ruby>foo|bar}{baz<rp>(</rp><rt>qux</rt><rp>)</rp></ruby></p>\n"
+    )]
+    #[case(
+        "{foo\\|bar}\\{baz|qux}",
+        "<p><ruby>foo|bar}{baz<rp>(</rp><rt>qux</rt><rp>)</rp></ruby></p>\n"
+    )]
+    #[case(
+        "Some stuff before      {foo      doo|bar            hello}  mid {baz|qux} after      words",
+        "<p>Some stuff before      <ruby>foo doo<rp>(</rp><rt>bar hello</rt><rp>)</rp></ruby>  mid <ruby>baz<rp>(</rp><rt>qux</rt><rp>)</rp></ruby> after      words</p>\n"
     )]
     fn test(#[case] md_str: &str, #[case] expected: &str) {
         let result = MARKDOWN_PARSER.parse(md_str).xrender();
